67a4f27867c7b981f30993137c230c08c052e9f3
Aleksandr <aleks.remiks@mail.ru>
2022-11-28 18:10:20 +0300

    add Example005ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, как же я скучал МАША, теперь мы снова можем кидать друзей, как крысы!");
+}
+else
+{
+    if(username.ToLower() == "даньчик")
+    {
+        Console.WriteLine("Не слушай братан, я тебя ни на кого не променяю! Ты же мой друг... Лучший друг!");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
+}
